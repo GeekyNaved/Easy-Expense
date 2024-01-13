45170de8b2c0738411cabcdd0bb11b7721ed6cea
GeekyNaved <navedahmed040@gmail.com>
2024-01-13 22:59:13 +0530

    adding tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div>
-      <h1>Easy Expense</h1>
+      <h1 className='text-center text-4xl text-violet-600'>Easy Expense</h1>
     </div>
   )
 }
