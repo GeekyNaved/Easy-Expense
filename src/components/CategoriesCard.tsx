36f24a7a9a8367e51ea94684302a0aa461686916
GeekyNaved <navedahmed040@gmail.com>
2024-05-16 15:58:44 +0530

    adding edit category dialog form (modal)

--- src/components/CategoriesCard.tsx
+++ src/components/CategoriesCard.tsx
@@ -1,21 +1,35 @@
 import { MdDelete, MdEdit } from "react-icons/md";
+import EditDialog from "./EditDialog";
+import { useState } from "react";
 
 interface CategoriesCardProps {
     name: string;
 }
 const CategoriesCard: React.FC<CategoriesCardProps> = ({ name }) => {
+    const [open, setOpen] = useState(false);
+
+    const handleClickOpen = (e) => {
+        // console.log('name', name)
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
     return (
         <div className="flex justify-between items-center rounded-xl bg-slate-50 shadow-lg px-5 py-5 mb-5">
             <p className="text-xl font-medium">{name}</p>
-            <div className="flex gap-5  md:gap-8">
+            <div className="flex gap-5 md:gap-8">
+                <EditDialog categoryName={name} isOpen={open} onClose={handleClose}>
+                    <button className="text-2xl flex flex-col items-center hover:underline" onClick={handleClickOpen}>
+                        <MdEdit />
+                        <p className="text-sm font-semibold">
+                            Edit
+                        </p>
+                    </button>
+                </EditDialog>
                 <button className="text-2xl flex flex-col items-center hover:underline">
-                    <MdEdit />
-                    <p className="text-sm font-semibold">
-                        Edit
-                    </p>
-                </button>
-                <button className="text-2xl flex flex-col items-center hover:underline">
-                    <MdDelete className="text-red-600"/>
+                    <MdDelete className="text-red-600" />
                     <p className="text-sm text-red-600 font-semibold">
                         Delete
                     </p>
---
+++ src/components/EditDialog.tsx
@@ -0,0 +1,68 @@
+// Edit category form dialog box (Modal)
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import { Fragment, useEffect, useState } from 'react';
+
+interface dialogProps {
+    categoryName: string;
+    isOpen: boolean;
+    onClose: () => void;
+    children: React.ReactNode;
+}
+
+const EditDialog: React.FC<dialogProps> = ({ categoryName, children, isOpen, onClose }) => {
+    const [name, setName] = useState('');
+
+    useEffect(() => {
+        setName(categoryName)
+    }, [isOpen]);
+
+    return (
+        <Fragment>
+            {children}
+            <Dialog
+                open={isOpen}
+                onClose={onClose}
+                PaperProps={{
+                    component: 'form',
+                    onSubmit: (event) => {
+                        event.preventDefault();
+                        const formData = new FormData(event.currentTarget);
+                        const formJson = Object.fromEntries(formData.entries());
+                        const email = formJson.email;
+                        console.log(email);
+                        onClose();
+                    },
+                }}
+            >
+                <DialogTitle>Edit Categories</DialogTitle>
+                <DialogContent>
+                    <TextField
+                        autoFocus
+                        required
+                        margin="dense"
+                        id="name"
+                        name="email"
+                        label="Enter category name"
+                        value={name}
+                        onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                            setName(event.target.value);
+                        }}
+                        type="text"
+                        fullWidth
+                        variant="standard"
+                    />
+                </DialogContent>
+                <DialogActions>
+                    <button className='text-red-500 px-4 py-2' onClick={onClose}>Cancel</button>
+                    <button className="font-medium px-4 py-2" type="submit">Save</button>
+                </DialogActions>
+            </Dialog>
+        </Fragment>
+    );
+}
+
+export default EditDialog;
